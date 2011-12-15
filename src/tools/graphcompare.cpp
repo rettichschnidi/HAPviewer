4315ab181fe8dc024e7adf99ab504a6b4f7e42e8
Reto Schneider <reto.schneider@9300wittenbach.ch>
2011-12-15 10:00:44 +0100

    Integrate haplibtest tests into cmake, added a - (read from stdin) option to graphcompare

--- src/tools/graphcompare.cpp
+++ src/tools/graphcompare.cpp
@@ -15,29 +15,39 @@ using namespace std;
 
 int main(int argc, char **argv) {
 	if (argc != 3) {
-		cerr << "Usage: ./graphcompare /path/to/graphA.dot /path/to/graphB.dot" << endl;
+		cerr << "Usage: ./graphcompare /path/to/graphA.dot /path/to/graphB.dot [or - to read from stdin instead of graphB.dot]" << endl;
 		exit(1);
 	}
 
 	try {
 		ifstream inputA(argv[1]);
-		ifstream inputB(argv[2]);
+		istream *inputB;
+
+		if(string(argv[2]) == "-") {
+			inputB = &cin;
+		} else {
+			inputB = new ifstream(argv[2]);
+		}
 
 		DotGraph graphA(inputA);
-		DotGraph graphB(inputB);
+		DotGraph graphB(*inputB);
 
-		bool equality = graphA.equalVerbose(graphB);
+		bool equality = graphA.diffGraphVerbose(graphB);
 		if (equality) {
-			cout << "Graphs are equal." << endl;
 			return 0;
 		} else {
+			if(graphA.isomorphism(graphB)) {
+				cout << "Graphs are isomorph." << endl;
+			} else {
+				cout << "Graphs are not isomorph." << endl;
+			}
 			return 1;
 		}
 	} catch (std::string &e) {
 		cerr << "Error: " << e << endl;
-		return 111;
+		return 2;
 	} catch (...) {
 		cerr << "Unknown error..." << endl;
-		return 112;
+		return 3;
 	}
 }
